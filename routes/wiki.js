875476797d26bfe48bef2b34ab09c35502970cc2
Jagnoor Grewal <noor.grewal@berkeley.edu>
2017-06-19 21:04:36 +0530

    Improving with Hooks

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,20 +1,34 @@
 const express = require('express');
 const router = express.Router();
+var models = require('../models');
+var Page = models.Page;
+var User = models.User;
 
 router.get('/', function(req, res, next) {
   res.redirect('/');
 });
 
 router.post('/', function(req, res, next) {
-  res.json(req.body);
-  //res.redirect('/');
+  var urlTitle = req.body.title.replace(/\s+/gi, '_').replace(/\W/g,'');
+  var page = Page.build({
+	  title: req.body.title, 
+	  urlTitle: urlTitle,
+	  content: req.body.content,
+	  status: req.body.status
+  });
+  page.save()
+	.then(function() {
+		res.redirect('/wiki/' + urlTitle);
+	});
 });
-// router.post('/wiki', function(req, res, next){
-//
-// });
+
 router.get('/add', function(req, res, next) {
   res.render('addpage');
 });
+
+
+
+
 
 
 module.exports = router;
